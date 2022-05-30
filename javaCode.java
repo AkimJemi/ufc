681865cad24105e79c62e97471dc478f8e311e2a
Akim <90965463+AkimJemi@users.noreply.github.com>
2022-05-30 16:57:01 +0900

    ArrayList 오림 차순/내림차순

---
+++ javaCode.java
@@ -0,0 +1,5 @@
+Integer[] integerArr = new Integer[] {1,3,5,2,4};                          
+String[] stringArr = new String[] {"A","C","B","E","D"};       
+     
+Arrays.sort(integerArr,Comparator.reverseOrder());     //내림차순
+Arrays.sort(stringArr,Collections.reverseOrder());    //내림차순
