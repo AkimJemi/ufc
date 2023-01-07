4b10f2203b410bb2cd44f88364a700741879121d
AkimJemi <rlawoals623@gmail.com>
2023-01-07 21:07:47 +0900

    add sql bing

---
+++ error/test1.js
@@ -0,0 +1,9 @@
+// 함수
+// 실행 가능한 데이터
+x = function(a,b){
+    return a + b;
+}
+
+x(3,56);
+x(4,234);
+console.log(x);
---
+++ javascript/test.js
@@ -0,0 +1,4 @@
+var a = function( a, b){
+    return a + b;
+}
+console.log(a(20,30));
