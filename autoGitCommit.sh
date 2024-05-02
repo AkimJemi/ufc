95f4b9475e0f3db2e526a83a3ed9db68469a3124
AkimJemi <rlawoals623@gmail.com>
2024-05-03 00:53:08 +0900

    echo 2024/05/03 00:53:08

--- autoGitCommit.sh
+++ autoGitCommit.sh
@@ -1,3 +1,17 @@
+REPOSITORY=$1
+TARGET=$2
+PARAM1=$3
+
+ufc=(language linux git)
+declare -A git_info=(
+  ["ufc"]="${ufc[@]}"
+  ["LeetCode_JEMIN"]="java"
+)
+
+
+
 git add .
 git commit -m "echo $(date +'%Y/%m/%d %H:%M:%S')"
 git push origin main
+
+
